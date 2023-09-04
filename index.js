ed7fe0202438222f6aad748313a5cd2c0e2eb75e
Syed Anas Ali <46435477+FirstOrangutan@users.noreply.github.com>
2023-09-04 12:06:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const array = ["1", "2", "3", "2", "1"];
+const revArray = array.reverse();
+// console.log(revArray);
+// console.log(array);
+
+// console.log(
+//   revArray.every((value, index) => {
+//     console.log(index);
+//     return value === array[index];
+//   })
+// );
